9ccae60af1bce4b8c3c67eafb1a38ada01fe3e6d
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-03-24 11:27:52 +0700

    menampilkan output ke layar

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -52,6 +52,10 @@ void insertionsort() {					// Procedure Insertionsort
 		arr[j + 1] = temp;				// 5. simpan nilai temp ke dalam arr[j+1
 
 
+		cout << "\nPass " << i << ": ";		// Output ke layar
+		for (int k = 0; k < n; n++) {		// Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";			// Output ke layar
+		}
 
 	}
 }

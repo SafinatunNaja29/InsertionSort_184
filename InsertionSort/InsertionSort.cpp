fa85e670482823de29540d64515b2b3e59b1d070
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-03-24 11:23:08 +0700

    mengulang looping while

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -42,7 +42,16 @@ void insertionsort() {					// Procedure Insertionsort
 
 		j = i - 1;						// 3. setting nilai j sama dengan i-1;
 
-		
+		while (j >= 0 && arr[j] > temp)	// 4. Looping while dimana nilai j lebih besar sama dengan 0 dan
+										// arr[j] lenih besar daripada temp
+		{
+			arr[j + 1] = arr[j];		// 4a. simpan arr[j] ke dalam variable arr[j+1]
+			j--;						// 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;				// 5. simpan nilai temp ke dalam arr[j+1
+
+
 
 	}
 }
